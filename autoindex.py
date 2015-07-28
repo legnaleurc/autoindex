f3cdd3d5053888c0fdd5d1a6a467bf65dc5ef284
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-07-29 03:52:16 +0900

    Add thread pool to read files.

--- autoindex.py
+++ autoindex.py
@@ -5,14 +5,17 @@ import os.path as op
 import logging
 import os
 import mimetypes
+import concurrent.futures as cf
 
-from tornado import ioloop, web, log, iostream, gen
+from tornado import ioloop, web, log, iostream, gen, concurrent
 
 
 class IndexHandler(web.RequestHandler):
 
-    def initialize(self, root, *args, **kwargs):
+    def initialize(self, root, pool, *args, **kwargs):
         self._root = root
+        self._pool = pool
+        self._loop = ioloop.IOLoop.current()
 
     @gen.coroutine
     def get(self, path):
@@ -29,16 +32,8 @@ class IndexHandler(web.RequestHandler):
                 self.set_header('Content-Type', type_)
             self.set_header('Content-Length', op.getsize(full_path))
 
-            with open(full_path, 'rb') as fin:
-                while True:
-                    chunk = fin.read(65536)
-                    if not chunk:
-                        return
-                    try:
-                        self.write(chunk)
-                        yield self.flush()
-                    except iostream.StreamClosedError:
-                        return
+            yield self._send_file(full_path)
+            return
 
         if op.isdir(full_path):
             items = os.listdir(full_path)
@@ -54,6 +49,23 @@ class IndexHandler(web.RequestHandler):
     def _is_file(self, base, name):
         return op.isfile(op.join(base, name))
 
+    @gen.coroutine
+    def _send_file(self, full_path):
+        with open(full_path, 'rb') as fin:
+            while True:
+                chunk = yield fin._read_chunk(65536)
+                if not chunk:
+                    return
+                try:
+                    self.write(chunk)
+                    yield self.flush()
+                except iostream.StreamClosedError:
+                    return
+
+    @concurrent.run_on_executor(io_loop='_loop', executor='_pool')
+    def _read_chunk(self, fin):
+        return fin.read(65536)
+
 
 def main(args=None):
     if args is None:
@@ -63,13 +75,20 @@ def main(args=None):
 
     main_loop = ioloop.IOLoop.instance()
 
+    pool = cf.ThreadPoolExecutor()
+
     application = web.Application([
-        (r'/(.*)', IndexHandler, {'root': '.'}),
+        (r'/(.*)', IndexHandler, {
+            'root': '.',
+            'pool': pool,
+        }),
     ], debug=True)
 
     application.listen(8000)
 
     main_loop.start()
+    pool.shutdown()
+    main_loop.close()
 
     return 0
 
